88442eaea5a16aaae7cf21062b4099bead3ac262
menachem <nmmlemmer@gmail.com>
2026-04-06 13:01:47 -0400

    Add deploy workflow, adjust relative links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  site: "https://menachemlemmer.github.io",
+  base: "/techelet-project",
 });
